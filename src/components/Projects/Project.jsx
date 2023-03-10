c453ab3a6b00e449b2773228426f5dd6779b14ca
Milad Amiri <miladmarketing282.com>
2023-03-11 01:36:09 +0800

    added

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -9,7 +9,7 @@ const Project = (props) => {
         <div className="disc">
             <h1>Description</h1>
             <p>{disc}
-            <a href="#">demo</a>
+            <a href="/">demo</a>
             </p>
         </div>
     </Container>
